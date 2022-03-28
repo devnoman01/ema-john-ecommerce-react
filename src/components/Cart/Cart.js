11ac56a37039fc4368e12b2d53b077dbcec68dac
devnoman01 <devnoman01@gmail.com>
2022-03-29 02:55:54 +0600

    React SPA E-Commerce

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+  let quantity = 1;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
   return (
-    <div>
+    <div className="cart">
       <h3>Order Summary</h3>
       <p>Item selected: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: ${shipping}</p>
+      <p>TAX: ${tax}</p>
+      <p>Grand Total: ${grandTotal}</p>
     </div>
   );
 };
